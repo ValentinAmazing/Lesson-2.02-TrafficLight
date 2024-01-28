00a24ff1d52a88f30709de483e30dbd4002f80ee
ValentinAmazing <podrezva@bk.ru>
2024-01-28 10:29:02 +0700

    Дизайн светофора в сториборде (3и "лампочки", кнопка) + ограничители адаптивные под разные устройства. Логика обработки событий при нажатии кнопки - переключает состояния лампочек.

--- Lesson 2.02 TrafficLight/ViewController.swift
+++ Lesson 2.02 TrafficLight/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var switchBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        initViews(redLightView, color: .red)
+        initViews(yellowLightView, color: .yellow)
+        initViews(greenLightView, color: .green)
+
+        switchBtn.backgroundColor = .systemBlue
+        switchBtn.layer.cornerRadius = 11
     }
 
-
+    @IBAction func switchBtn(_ sender: UIButton) {
+        let alpha03: CGFloat = 0.3
+        let alpha1: CGFloat = 1
+        
+        if redLightView.alpha == alpha1 {
+            redLightView.alpha = alpha03
+            yellowLightView.alpha = alpha1
+        } else if yellowLightView.alpha == alpha1 {
+            yellowLightView.alpha = alpha03
+            greenLightView.alpha = alpha1
+        } else if greenLightView.alpha == alpha1 {
+            greenLightView.alpha = alpha03
+            redLightView.alpha = alpha1
+        } else {
+            sender.setTitle("NEXT", for: .normal)
+            redLightView.alpha = alpha1
+        }
+    }
+    
 }
 
+func initViews(_ view: UIView, color: UIColor) {
+    view.backgroundColor = color
+    view.alpha = 0.3
+    view.layer.cornerRadius = view.frame.height / 2
+}
